8d279ce34b5829a04b0dca778d3fbc26e1305b39
23scurtu <23scurtuvictor@gmail.com>
2019-07-23 19:41:37 -0400

    Add final scene

--- Project/Assets/demo_scenes/final_scene.lua
+++ Project/Assets/demo_scenes/final_scene.lua
@@ -1,4 +1,4 @@
-SHOW_EFFECT = true
+-- SHOW_EFFECT = true
 
 s = 1/8--1/8
 wall_r = 0.1
@@ -6,13 +6,14 @@ r = {wall_r,wall_r, wall_r}
 mat1 = gr.cmaterial({1,1,0.3}, {0.1,0.1,0.1}, 25, 0, 0, 0, 0, 0)
 -- mat4 = gr.cmaterial({1, 1, 1}, {0.1, 0.1, 0.1}, 25, 0.8, 1.1, 0, 0)
 mat2 = gr.material({0.5,0.25,0}, {0.1,0.1,0.1}, 25)
+red_mat = gr.material({1,0.15,0.15}, r, 25, 0)
 
 clear_transparent_13_mat = gr.cmaterial({0,0,0}, {0.1,0.1,0.1}, 100, 1, 1.3, 0, 0)
 
 clear_transparent_1_mat = gr.cmaterial({0.9,0.9,0.9}, {1,1,1}, 100, 1, 1.3, 0, 0)
 
-mirror = gr.cmaterial({0.9,0.9,0.9}, {0.95,0.95,0.95}, 100)
-double_sided_mirror = gr.cmaterial({0.9,0.9,0.9}, {0.95,0.95,0.95}, 100, 0.1, 1, 0, 0)
+mirror = gr.material({0.2,0.2,0.2}, {0.75,0.75,0.75}, 100)
+double_sided_mirror = gr.cmaterial({0.9,0.9,0.9}, {0.9,0.9,0.9}, 100, 1, 1, 0, 0)
 
 white_mat = gr.material({1, 0.95, 0.8}, r, 25, 0)
 
@@ -42,16 +43,31 @@ s1:set_material(clear_transparent_13_mat)
 
 -- Infinity mirror
 inf_mirror = gr.node('infinity_mirror')
-inf_mirror:scale(25,25,25)
-inf_mirror:translate(50,25, 0)
+inf_mirror:scale(50,50,25)
+inf_mirror:translate(-75,25, -200)
 root:add_child(inf_mirror)
 
 s4 = gr.cube('s1', {0, 0, 0})
--- s4:scale(50,50,50)
+s4:scale(25,25,1)
 -- s4:rotate('X', -90)
--- s4:rotate('Y', 25)
--- s4:translate(50,0, 125)
-inf_mirror.add_child(s4)
+-- s4:rotate('Y', -25)
+s4:translate(-12.5,-5, 25)
+s4:set_material(mirror)
+inf_mirror:add_child(s4)
+
+s5 = gr.cube('s1', {0, 0, 0})
+s5:scale(3,3,1)
+-- s4:rotate('X', -90)
+s5:rotate('Y', 0)
+s5:translate(0,0, 0)
+s5:set_material(mirror)
+inf_mirror:add_child(s5)
+
+s6 = gr.sphere('ball', {0,0,0})
+s6:scale(25,25,25)
+s6:translate(50,200,0)
+s6:set_material(red_mat)
+root:add_child(s6)
 
 
 -- s2 = gr.sphere('s1', {0, 0, 0})
@@ -106,15 +122,15 @@ else
 	root:add_child(white_area_light)
 end
 
-if SHOW_EFFECT then
-light_pos = {-50, 190, 100}
-else
-light_pos = {50, 190, 100}
-end
+
+light_pos = {-100, 500, 100}
+
 
 white_light = gr.light(light_pos, {point_light_brightness,point_light_brightness,point_light_brightness}, {1.2,0,0})--{0,0,0.00008})
 
-size = 1024
+size = 4096
+
+gr.render_options(20,true, 2, 3,2,2, {4,2}, 250)
 
 gr.render(root, 'images/output.png', size, size,-- 512, 512, --256, 256, --1024, 1024,
 	  {0, 100, 350}, {0, 100, 0}, {0, 1, 0}, 50,
